8da81812d49cd29d70515ca1ea1958818275b4f7
yogita-gkmit <yogita@gkmit.co>
2024-11-03 10:08:53 +0530

    feat: db setup with migrations and models in postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { OperationsModule } from './modules/operations/operations.module';
 
 @Module({
-  imports: [],
+  imports: [OperationsModule],
   controllers: [AppController],
   providers: [AppService],
 })
